5caf10b94b6b98d1b6c521f7ab4e4a16f785b932
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-06-27 20:25:19 -0400

    connect server.js with mysql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,21 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mysql username,
+        user: 'root',
+        // your mysql password,
+        password: 'zaq12wsx',
+        database: 'election'
+    },
+    console.log("connected to the eleciton database")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 // default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
